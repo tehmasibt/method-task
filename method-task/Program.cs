587f29ea9c7a5cc841a7d1c82a64071f1774539e
thmasibtagiyev0@gmail.com <tahmasibmt@code.edu.az>
2024-02-21 09:08:00 +0400

    Add project files.

---
+++ method-task/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace method_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            //Console.WriteLine(Calculate(4,3));
+            //Console.WriteLine(Arr(12,2,4,6,23));
+            //Console.WriteLine(Tek(5,10));
+            //Console.WriteLine(Quvv(6));
+        }
+        #region task-1
+        //static int Calculate(int a,int b)
+        //{
+        //    return a/b;
+        //}
+        #endregion
+        #region task-2
+        //static int Arr(params int[] nums)
+        //{
+        //    int sum = 0;
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+        //        sum += nums[i];
+        //    }
+        //    return sum;
+        //}
+        #endregion
+        #region task-3
+        //static int Tek(int n,int m)
+        //{
+        //    int count = 0;
+        //    for (int i = n; i < m; i++)
+        //    {
+        //        if (i % 2 == 1)
+        //        {
+        //            count++;
+        //        }
+        //    }
+        //    return count;
+        //}
+        #endregion
+        #region task-5      
+        //static string Quvv(double n)
+        //{
+
+        //    while (n>1)
+        //    {
+        //        n /= 2;
+        //    }
+        //    if (n == 1)
+        //    {
+        //        return "quvvetidir";
+        //    }
+        //    else 
+        //    {
+        //        return "quvveti deyil";
+        //    }
+        //}
+        #endregion
+
+    }
+}
